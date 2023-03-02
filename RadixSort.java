f09e8d0a9412eedc6737ae7e621c83d6e9bc8b47
TKCoder13 <TKCoding13@gmail.com>
2023-03-01 19:12:27 -0800

    completed the RadixSort

--- RadixSort.java
+++ RadixSort.java
@@ -1,34 +1,47 @@
 public class RadixSort {
 
-    public static int[] radixSort(int[] list) {
-        int buckets[][] = new int[10][list.length];
-        int bucketCounts[];
-        int keySize = 10; // refer to pusedocode in CSC 310 Notes
-        int shift = 1;
-        for (int i = 0; i < keySize; i++) {
-            bucketCounts = new int[10];
-            for (int j = 0; j < list.length; j++) {
-                int bn = (list[j] / shift) % 10; // bn --> bucketNumber
-                buckets[bn][bucketCounts[bn]++] = list[j];
-                // or you can do the following
-                // buckets[bn][bucketCounts[bn]] = list[j];
-                // ++bucketCounts[bn];
+    public static String[] radixSort(String[] list) { // Passing in a String[] list instead of int[]
+        int keySize = 0;
+        int N = list.length;
+        // Finding the longest string in the list
+        for (int i = 0; i < list.length; i++) {
+            if (keySize < list[i].length()) {
+                keySize = list[i].length();
             }
-            list = combineBuckets(buckets, bucketCounts);
-            shift *= 10;
+        }
+        //Padding shorter strings with spaces
+        for (int i = 0; i < list.length; i++) {
+            while (list[i].length() < keySize) {
+                list[i] = list[i] + " ";
+            }
+        }
+        
+        String buckets[][] = new String[27][list.length];
+        int bucketCounts[];
+        int shift = keySize - 1;
+
+        // refer to pusedocode in CSC 310 Notes
+        for (int i = 0; i < keySize; i++) {
+            bucketCounts = new int[27]; // a-z + space
+            for (int j = 0; j < list.length; j++) {
+                int bn = list[j].charAt(shift); // bn --> bucketNumber
+                if (bn == 32) { // space --> 0
+                    buckets[0][bucketCounts[0]++] = list[j];
+                } else { // a-z
+                    buckets[bn - 96][bucketCounts[bn - 96]++] = list[j];
+                }
+            }
+            list = combineBuckets(buckets, bucketCounts, N);
+            shift--;
         }
         return list;
     }
 
-    private static int[] combineBuckets(int buckets[][], int bucketCounts[]) { // You can also pass in N as an argument, but here we calc in the method.
-        int N = 0;
-        for (int i = 0; i < bucketCounts.length; i++) {
-            N += bucketCounts[i];
-        }
-        int combinedBuckets[] = new int[N];
+    public static String[] combineBuckets(String buckets[][], int bucketCounts[], int N) {
+        String combinedBuckets[] = new String[N];
         int j = 0;
-        for (int bn = 0; bn < buckets.length; ++bn) {
-            for (int b = 0; b < bucketCounts[bn]; ++b) {
+        for (int bn = 0; bn < buckets.length; bn++) {
+            for (int b = 0; b < bucketCounts[bn]; b++) {
                 combinedBuckets[j++] = buckets[bn][b];
             }
         }
@@ -36,10 +49,17 @@ public class RadixSort {
     }
 
     public static void main(String args[]) {
-        int[] list = {10,9,8,5,1,2,3,4};
-        int[] test = radixSort(list);
-        for (int i = 0; i < list.length; i++) {
-            System.out.println(test[i]);
+        String[] unsorted = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"};
+        String[] sorted = radixSort(unsorted);
+
+        System.out.println("Unsorted List");
+        for (int i = 0; i < unsorted.length; i++) {
+            System.out.println(i + ": \t" + unsorted[i]);
         }
+        System.out.println("\nSorted List");
+        for (int i = 0; i < sorted.length; i++) {
+            System.out.println(i + ": \t" + sorted[i]);
+        }
+        
     }
 }
